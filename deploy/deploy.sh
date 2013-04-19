d1c63cd4e19666819e81d05222843ed90052abad
djmattyg007 <mwgam36@gmail.com>
2013-04-20 01:19:19 +1000

    Switched back to an updated exclude statement.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ ROOT="`dirname $0`/.."
 SOURCE="$ROOT"
 DEST="/var/www/dabr/"
 
-EXCLUDE="--exclude=\".git**\" --exclude=\"deploy/***\""
+EXCLUDE="--exclude .git* --exclude deploy"
 FILTER="--filter=\"- .git - .gitignore - deploy/\""
 
 DRYRUN="--dry-run"
@@ -16,5 +16,5 @@ if [[ $# -eq 1 ]]; then
 	fi
 fi
 
-echo $RSYNC $DRYRUN --itemize-changes --delete -rtv $FILTER $SOURCE $DEST
-$RSYNC $DRYRUN --itemize-changes --delete -rtv $FILTER $SOURCE $DEST
+echo $RSYNC $DRYRUN --itemize-changes --delete -rtv $EXCLUDE $SOURCE $DEST
+$RSYNC $DRYRUN --itemize-changes --delete -rtv $EXCLUDE $SOURCE $DEST
