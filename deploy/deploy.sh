076b2984d2c949b2b4e7a02ebf15c4b2f7114afb
djmattyg007 <mwgam36@gmail.com>
2013-04-20 01:02:43 +1000

    Added initial deployment script.

---
+++ deploy/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RSYNC="`/usr/bin/which rsync`"
+ROOT="`dirname $0`/.."
+SOURCE="$ROOT"
+DEST="/var/www/dabr/"
+
+EXCLUDE="--exclude='.git*' --exclude='deploy/'"
+
+DRYRUN="--dry-run"
+
+if [[ $# -eq 1 ]]; then
+	if [[ "$1" == "-f" ]]; then
+		DRYRUN=""
+	fi
+fi
+
+$RSYNC $DRYRUN --itemize-changes --delete -rtv $EXCLUDE $SOURCE $DEST
