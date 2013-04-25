556a7b5a4c1e1c6625cf3e8c055444996be18469
djmattyg007 <mwgam36@gmail.com>
2013-04-26 08:41:27 +1000

    Added deployment script for development site.
    Renamed original deployment script as a result.

---
+++ deploy/dev-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RSYNC="`/usr/bin/which rsync`"
+ROOT="`dirname $0`/.."
+SOURCE="$ROOT"
+DEST="/var/www/dabr-dev/"
+
+EXCLUDE="--exclude .git* --exclude deploy --exclude README.md"
+
+DRYRUN="--dry-run"
+
+if [[ $# -eq 1 ]]; then
+	if [[ "$1" == "-f" ]]; then
+		DRYRUN=""
+	fi
+fi
+
+echo $RSYNC $DRYRUN --itemize-changes --delete -rtv $EXCLUDE $SOURCE $DEST
+$RSYNC $DRYRUN --itemize-changes --delete -rtv $EXCLUDE $SOURCE $DEST
